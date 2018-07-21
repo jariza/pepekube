7e65e2bd78ac8b446b6925afbea7ce09f10a7e9a
jariza <jariza@ieee.org>
2018-07-21 16:09:43 +0200

    Include time data

--- index.php
+++ index.php
@@ -13,21 +13,25 @@
 $data=(json_decode(file_get_contents("http://houseapi.default.svc.cluster.local:31500/weather"), true));
 foreach ($data['wd'] as $element) {
 	if ($element['name'] == 'Indoor') {
-		echo "<h2>Indoors</h2>\n<ul>\n";
-		echo "<li>Temperature: ".round($element['temperature'], 2)."ºC</li>\n";
-		echo "<li>Humidity: ".$element['humidity']."%</li>\n";
-		echo "<li>Pressure: ".round($element['pressure'], 2)."mbar</li>\n";
-		echo "<li>CO2: ".$element['co2']."ppm</li>\n";
-		echo "<li>Noise: ".$element['noise']."dB</li>\n";
-		echo "<li>Link status: ".$element['link_status']."%</li>\n";
+		if ((time() - $element['time_utc']) > 600) {$freshness = ' style="color: red"';} else {$freshness = '';}
+		echo "<h2$freshness>Indoors</h2>\n<ul>\n";
+		echo "<li$freshness>Temperature: ".round($element['temperature'], 2)."ºC</li>\n";
+		echo "<li$freshness>Humidity: ".$element['humidity']."%</li>\n";
+		echo "<li$freshness>Pressure: ".round($element['pressure'], 2)."mbar</li>\n";
+		echo "<li$freshness>CO2: ".$element['co2']."ppm</li>\n";
+		echo "<li$freshness>Noise: ".$element['noise']."dB</li>\n";
+		echo "<li$freshness>Link status: ".$element['link_status']."%</li>\n";
+		echo "<li$freshness>Time: ".date('H:i:s, j/M/Y', $element['time_utc'])."</li>\n";
 		echo "</ul>\n";
 	}
 	if ($element['name'] == 'Exterior') {
-		echo "<h2>Outdoors</h2>\n<ul>\n";
-		echo "<li>Temperature: ".round($element['temperature'], 2)."ºC</li>\n";
-		echo "<li>Humidity: ".$element['humidity']."%</li>\n";
-		echo "<li>Link status: ".$element['link_status']."%</li>\n";
-		echo "<li>Battery: ".$element['battery_percent']."%</li>\n";
+		if ((time() - $element['last_seen']) > 600) {$freshness = ' style="color: red"';} else {$freshness = '';}
+		echo "<h2$freshness>Outdoors</h2>\n<ul>\n";
+		echo "<li$freshness>Temperature: ".round($element['temperature'], 2)."ºC</li>\n";
+		echo "<li$freshness>Humidity: ".$element['humidity']."%</li>\n";
+		echo "<li$freshness>Link status: ".$element['link_status']."%</li>\n";
+		echo "<li$freshness>Battery: ".$element['battery_percent']."%</li>\n";
+		echo "<li$freshness>Last seen: ".date('H:i:s, j/M/Y', $element['last_seen'])."</li>\n";
 		echo "</ul>\n";
 	}
 }
