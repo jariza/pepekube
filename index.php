9a53a4761f8afd6702dd638e4becf1ba0007aa8c
jariza <jariza@ieee.org>
2018-09-01 12:26:37 +0200

    Improved webpage and traffic camera

--- index.php
+++ index.php
@@ -13,28 +13,48 @@
 $data=(json_decode(file_get_contents("http://houseapi.default.svc.cluster.local:31500/weather"), true));
 foreach ($data['wd'] as $element) {
 	if ($element['name'] == 'Indoor') {
-		if ((time() - $element['time_utc']) > 600) {$freshness = ' style="color: red"';} else {$freshness = '';}
-		echo "<h2$freshness>Indoors</h2>\n<ul>\n";
-		echo "<li$freshness>Temperature: ".round($element['temperature'], 2)."ºC</li>\n";
-		echo "<li$freshness>Humidity: ".$element['humidity']."%</li>\n";
-		echo "<li$freshness>Pressure: ".round($element['pressure'], 2)."mbar</li>\n";
-		echo "<li$freshness>CO2: ".$element['co2']."ppm</li>\n";
-		echo "<li$freshness>Noise: ".$element['noise']."dB</li>\n";
-		echo "<li$freshness>Link status: ".$element['link_status']."%</li>\n";
-		echo "<li$freshness>Time: ".date('H:i:s, j/M/Y', $element['time_utc'])."</li>\n";
-		echo "</ul>\n";
+		if ((time() - $element['time_utc']) > 660) {$freshness = ' style="color: red"';} else {$freshness = '';}
+		echo "\t<h2$freshness>Indoors</h2>\n\t<ul>\n";
+		echo "\t\t<li$freshness>Temperature: ".round($element['temperature'], 2)."ºC</li>\n";
+		echo "\t\t<li$freshness>Humidity: ".$element['humidity']."%</li>\n";
+		echo "\t\t<li$freshness>Pressure: ".round($element['pressure'], 2)."mbar</li>\n";
+		echo "\t\t<li$freshness>CO2: ".$element['co2']."ppm</li>\n";
+		echo "\t\t<li$freshness>Noise: ".$element['noise']."dB</li>\n";
+		echo "\t\t<li$freshness>Link status: ".$element['link_status']."%</li>\n";
+		echo "\t\t<li id=\"time\"$freshness></li>\n";
+		echo "\t</ul>\n";
+		echo "\t<script>\n";
+		echo "\t\tdtime = new Date(".date('\'M/j/Y G:i:s \U\T\C\'', $element['time_utc']).")\n";
+		echo "\t\tdocument.getElementById('time').innerHTML = 'Time: ' + dtime.toString()\n";
+		echo "\t</script>\n";
 	}
 	if ($element['name'] == 'Exterior') {
-		if ((time() - $element['last_seen']) > 600) {$freshness = ' style="color: red"';} else {$freshness = '';}
-		echo "<h2$freshness>Outdoors</h2>\n<ul>\n";
-		echo "<li$freshness>Temperature: ".round($element['temperature'], 2)."ºC</li>\n";
-		echo "<li$freshness>Humidity: ".$element['humidity']."%</li>\n";
-		echo "<li$freshness>Link status: ".$element['link_status']."%</li>\n";
-		echo "<li$freshness>Battery: ".$element['battery_percent']."%</li>\n";
-		echo "<li$freshness>Last seen: ".date('H:i:s, j/M/Y', $element['last_seen'])."</li>\n";
-		echo "</ul>\n";
+		if ((time() - $element['last_seen']) > 660) {$freshness = ' style="color: red"';} else {$freshness = '';}
+		echo "\t<h2$freshness>Outdoors</h2>\n\t<ul>\n";
+		echo "\t\t<li$freshness>Temperature: ".round($element['temperature'], 2)."ºC</li>\n";
+		echo "\t\t<li$freshness>Humidity: ".$element['humidity']."%</li>\n";
+		echo "\t\t<li$freshness>Link status: ".$element['link_status']."%</li>\n";
+		echo "\t\t<li$freshness>Battery: ".$element['battery_percent']."%</li>\n";
+		echo "\t\t<li id=\"lastseen\"$freshness></li>\n";
+		echo "\t</ul>\n";
+		echo "\t<script>\n";
+		echo "\t\tdlastseen = new Date(".date('\'M/j/Y G:i:s \U\T\C\'', $element['last_seen']).")\n";
+		echo "\t\tdocument.getElementById('lastseen').innerHTML = 'Time: ' + dlastseen.toString()\n";
+		echo "\t</script>\n";
 	}
 }
 ?>
-</html>
+	<h2>Camera</h2>
+	<div class="camera">
+		<img id="traffic_camera" src='http://ctrafico.movilidad.malaga.eu/cst_ctrafico/camara1070.jpg' /><br />
+		<a href="http://movilidad.malaga.eu/es/servicios/camaras-de-trafico/">Moar camera</a>
+	</div>
+	<script>
+		function reloadImage() {
+			document.getElementById("traffic_camera").src = 'http://ctrafico.movilidad.malaga.eu/cst_ctrafico/camara1070.jpg' + '?dummy='+(new Date()).getTime()
+		}
+		var camaraTimer=setInterval('reloadImage()',(5*1000));
+	</script>
+
 </body>
+</html>
